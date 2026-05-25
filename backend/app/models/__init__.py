46f07eb17e604f13220ce1a76aad4fd52789d894
João Gabriel <jota.amaralmiranda@gmail.com>
2026-05-25 15:27:12 -0300

    refactor(models): implementa o uso de models para criacao de tabelas

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,5 @@
+from .user import User
+from .media import Media
+from .settings import Settings
+
+__all__ = ["User", "Media", "Settings"]
---
+++ backend/app/models/media.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from datetime import datetime
+from database import Base
+
+class Media(Base):
+    __tablename__ = "media"
+
+    id = Column(Integer, primary_key=True, index=True)
+    filename = Column(String, nullable=False)
+    original_name = Column(String, nullable=False)
+    type = Column(String, nullable=False)  # "image" or "video"
+    duration = Column(Integer, default=10)  # seconds
+    active = Column(Boolean, default=True)
+    order = Column(Integer, nullable=False)
+    uploaded_at = Column(DateTime, default=datetime.utcnow)
+    url = Column(String, nullable=False)
